096152a280a7a1bea15102854a971053ffbc6e29
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-25 00:09:23 +0000

    Finish Practice

--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -1,8 +1,11 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
-def sort_names(arrTemp):
-    name = ""
-    for i in arrTemp:
-        return name[i]
+def sort_names(arrayFunct):    
+    names = []
+    for i in arrayFunct:
+        names.append(i)
+    names.sort()
+    return names 
+    
 
 print(sort_names(names))
